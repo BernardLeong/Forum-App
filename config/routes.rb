7db03000e2968f3a9a86d8e80c19a1b6a684657a
Bernard <bernardleongqijie@gmail.com>
2018-02-24 20:45:00 +0800

    devise set up finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-  root 'home#index'
-
-  
-
-  
-
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+
+  root 'home#index'  
+  devise_for :users, controllers: {registrations: 'registrations'}
 
 
 
