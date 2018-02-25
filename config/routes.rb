f946250ce02b9406cd8e9a0b41ed17eb5a2b9351
Bernard <bernardleongqijie@gmail.com>
2018-02-25 14:06:32 +0800

    add scoffald

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :discussions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
 
-  root 'home#index'  
+  root 'discussions#index'  
   devise_for :users, controllers: {registrations: 'registrations'}
 
 
